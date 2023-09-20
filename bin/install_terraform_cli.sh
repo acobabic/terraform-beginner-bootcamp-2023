1ba1fc8992084b9aa0698cac6bde88344a3749e5
Aleksandar Babic <aleksandarbabic989@gmail.com>
2023-09-20 08:21:28 +0000

    #3 created install_terraform_cli.sh scrip, updated .gitpod.yml to use the script, documented the process

---
+++ bin/install_terraform_cli.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#Install software prerequisites
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+
+#Download and add Hashicopr signing key
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+#Add Hashicorp repository
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+#Install Terraform CLI 
+sudo apt-get update && sudo apt-get install -y terraform
