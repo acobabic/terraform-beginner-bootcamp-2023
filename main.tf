a54165aef8606296daa448545c3cb695f0cc13f4
acobabic <92812231+acobabic@users.noreply.github.com>
2023-09-22 14:41:27 +0200

    #13 migrated state to terraform cloud (#14)

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "BBA-LEARN"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+
   required_providers {
     random = {
       source  = "hashicorp/random"
