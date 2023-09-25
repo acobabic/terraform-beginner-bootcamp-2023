ebe8e4c020f9e77736afc07a4675dcae6ab82863
Aleksandar Babic <aleksandarbabic989@gmail.com>
2023-09-25 19:56:23 +0000

    #21 fixed a typo in variable condition reference

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "user_uuid" {
   type    = string
   description = "User UUID tag to associate with resources"
   validation {
-    condition = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid_tag))
+    condition = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid))
     error_message = "Invalid user UUID tag format. It should be a valid UUID."
   }
 }
