ce51d129987642960634b3c63be665688974b6ad
acobabic <92812231+acobabic@users.noreply.github.com>
2023-09-28 21:57:27 +0200

    #29 created cloudfron distribution, origin access and bucket policy (#30)

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -4,6 +4,11 @@ output "website_bucket_name" {
 }
 
 output "website_endpoint" {
-  description = "URL of our staticly hosted website"
+  description = "URL of our statically hosted website"
   value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
+
+output "s3_distribution_domain_name" {
+  description = "Domain name of our CloudFront Distribution used for our statically hosted website"
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
